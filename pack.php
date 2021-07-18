31555d34927eb8e4bb16aa9df9358fcfaab2e88a
liangjian <liangjian@oliveche.com>
2021-07-19 07:35:40 +0800

    update new arch

--- pack.php
+++ pack.php
@@ -1,5 +1,155 @@
 <?php
 
+class TBase
+{
+	function encode($obj) {
+		$fmt = '';
+		$params = [null];
+		$values = $this->onEncode($obj);
+		$this->getEncodeParams($values, $fmt, $params);
+		$params[0] = $fmt;
+		// var_dump($params);
+		return call_user_func_array("pack", $params);
+	}
+	function decode($pack, &$pos=0) {
+		$val = $this->onDecode();
+		foreach ($val as $k => &$v) {
+			if ($v == "n") {
+				$v = self::readInt16($pack, $pos);
+			}
+			else if (substr($v, 0, 2) == "T_") {
+				if (substr($v, -1) == "*") { // isArray
+					$v = substr($v, 0, -1);
+					if (!class_exists($v))
+						throw new Exception("bad class $v");
+					$cls = new $v;
+					$len = strlen($pack) - $pos;
+					$v = [];
+					while ($pos < $len) {
+						$e = $cls->decode($pack, $pos);
+						$v[] = $e;
+					}
+				}
+				else {
+					if (!class_exists($v))
+						throw new Exception("bad class $v");
+					$v = (new $v)->decode($pack, $pos);
+				}
+			}
+			else {
+				throw new Exception("unsupported decode mark or class `$v`");
+			}
+		}
+		return $val;
+	}
+
+	protected function onEncode($obj) {
+	}
+	protected function onDecode() {
+	}
+
+	private function getEncodeParams($values, &$fmt, &$param)
+	{
+		$cnt = count($values);
+		if ($cnt < 2 || $cnt % 2 != 0)
+			throw new Exception("bad arguments for writeBin");
+
+		for ($i=0; $i<$cnt; $i+=2) {
+			$k = $values[$i];
+			$v = $values[$i+1];
+			if (substr($k, 0, 2) == "T_") {
+				$isArray = false;
+				if (substr($k, -1) == "*") {
+					$isArray = true;
+					$k = substr($k, 0, -1);
+				}
+				if (!class_exists($k))
+					throw new Exception("bad class $k");
+				if ($isArray) {
+					foreach ($v as $e) {
+						$rv = (new $k)->onEncode($e);
+						$this->getEncodeParams($rv, $fmt, $param);
+					}
+				}
+				else {
+					$rv = (new $k)->onEncode($v);
+					$this->getEncodeParams($rv, $fmt, $param);
+				}
+			}
+			else {
+				$fmt .= $k;
+				array_push($param, $v);
+			}
+		}
+	}
+
+	static function readInt16($pack, &$pos)
+	{
+		$len = 2;
+		$o = unpack("na", substr($pack, $pos, $len));
+		$pos += 2;
+		return $o["a"];
+	}
+}
+
+class T_S7Str extends TBase
+{
+	protected function onEncode($obj) {
+		$len = strlen($obj);
+		return [
+			"c", $len,
+			"c", $len,
+			"a".$len, $obj
+		];
+	}
+
+	function decode($pack, &$pos=0)
+	{
+		$o = unpack("ccap/clen", substr($pack, $pos, 2));
+		$pos += 2;
+		$str = substr($pack, $pos, $o["len"]);
+		$pos += $o["len"];
+		return $str;
+	}
+}
+
+class T_Carton extends TBase
+{
+	protected function onEncode($obj) {
+		return [
+			"T_S7Str", $obj["code"],
+			"T_S7Str", $obj["type"],
+			"n", $obj["qty"]
+		];
+	}
+	protected function onDecode() {
+		return [
+			"code" => "T_S7Str",
+			"type" => "T_S7Str",
+			"qty" => "n"
+		];
+	}
+}
+
+class T_ArrivePackage extends TBase
+{
+	protected function onEncode($obj) {
+		return [
+			"T_S7Str", $obj["ac"],
+			"T_S7Str", $obj["area"],
+			"T_Carton*", $obj["cartonList"]
+		];
+	}
+
+	protected function onDecode() {
+		return [
+			"ac" => "T_S7Str",
+			"area" => "T_S7Str",
+			"cartonList" => "T_Carton*"
+		];
+	}
+}
+
 $pack = [
 	"ac" => "arrive",
 	"area" => "A",
@@ -17,126 +167,12 @@ $pack = [
 	]
 ];
 
-/*
-static function decodeStr($s, &$pos) {
-	$s1 = substr($s, $pos, 2);
-	$o = unpack("ccap/clen", $s1);
-	$s2 = substr($s, $pos+2, $o["len"]);
-	$pos += $o["len"] + 2;
-	return $s2;//unpack("a" . $o["len"] . "str", $s2)["str"];
-}
-*/
-
-function readInt16($pack, &$pos)
-{
-	$len = 2;
-	$o = unpack("na", substr($pack, $pos, $len));
-	$pos += 2;
-	return $o["a"];
-}
-
-function readS7Str($pack, &$pos)
-{
-	$o = unpack("ccap/clen", substr($pack, $pos, 2));
-	$pos += 2;
-	$str = substr($pack, $pos, $o["len"]);
-	$pos += $o["len"];
-	return $str;
-}
-
-function writeBin()
-{
-	$values = func_get_args();
-	$cnt = count($values);
-	if ($cnt < 2 || $cnt % 2 != 0)
-		throw "bad arguments for writeBin";
-
-	$fmt = '';
-	$param = [null];
-	for ($i=0; $i<$cnt; $i+=2) {
-		$k = $values[$i];
-		$v = $values[$i+1];
-		if ($k == "S7Str") {
-			$len = strlen($v);
-			$fmt .= 'cca' . $len;
-			array_push($param, $len, $len, $v);
-		}
-		else {
-			$fmt .= $k;
-			array_push($param, $v);
-		}
-	}
-	$param[0] = $fmt;
-	var_dump($param);
-	return call_user_func_array("pack", $param);
-}
-
-class Carton
-{
-	static function encode($obj) {
-		return writeBin(
-			"S7Str", $obj["code"],
-			"S7Str", $obj["type"],
-			"n", $obj["qty"]
-		);
-	}
-	static function decode($pack, &$pos=0) {
-		return [
-			"code" => readS7Str($pack, $pos),
-			"type" => readS7Str($pack, $pos),
-			"qty" => readInt16($pack, $pos)
-		];
-	}
-}
-
-class ArrivePackage
-{
-	static function encode($obj) {
-		$val = writeBin(
-			"S7Str", $obj["ac"],
-			"S7Str", $obj["area"]
-		);
-
-		foreach ($obj["cartonList"] as $e) {
-			$val .= Carton::encode($e);
-		}
-		return $val;
-	}
-
-	static function decode($pack, &$pos=0) {
-		$val = [
-			"ac" => readS7Str($pack, $pos),
-			"area" => readS7Str($pack, $pos),
-			"cartonList" => []
-		];
-		$len = strlen($pack);
-		while ($pos < $len) {
-			$e = Carton::decode($pack, $pos);
-			$val["cartonList"][] = $e;
-		}
-		return $val;
-	}
-}
-
-$x = Carton::encode($pack["cartonList"][1]);
-$y = Carton::decode($x);
+$p = new T_Carton();
+$x = $p->encode($pack["cartonList"][1]);
+$y = $p->decode($x);
 var_dump($y);
 
-$x = ArrivePackage::encode($pack);
-$y = ArrivePackage::decode($x);
+$p = new T_ArrivePackage();
+$x = $p->encode($pack);
+$y = $p->decode($x);
 var_dump($y);
-
-/*
-$Carton_FMT = "a50a2n";
-$Pack_FMT = "a8c[Carton_FMT]";
-
-$fmt = "a8c[Carton_FMT]";
-
-pack($
-
-# echo(pack("N", 0xffff3386));
-# echo(pack("N", 0x99887766));
-# exit();
-$x = file_get_contents("1");
-var_dump(unpack("Ccap/Cnum/a7str", $x));
-*/
